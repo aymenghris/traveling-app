2de53908f15b24f7ad65953dcaca1e50a22827bf
aymenghris <aymengharis@gmail.com>
2025-07-02 10:36:59 +0100

    add constants for images, navigation links, features, footer, and social media

---
+++ constants/index.ts
@@ -0,0 +1,124 @@
+// IMAGES
+export const IMAGES = {
+    camp: {
+        quote: '/camp/quote.svg',
+        map: '/camp/folded-map.svg',
+    },
+    hero: {
+        close: '/hero/close.svg',
+        play: '/hero/play.svg',
+        star: '/hero/star.svg',
+    },
+    getapp: {
+        phones: '/get-app/phones.png',
+        apple: '/get-app/apple.svg',
+        android: '/get-app/android.svg',
+    },
+    guide: {
+        boats: '/guide/boats.png',
+        meter: '/guide/meter.svg',
+    },
+    navbar: {
+        menu: '/navbar/menu.svg',
+        user: '/navbar/user.svg',
+    },
+    shared: {
+        logo: '/shared/app-logo.svg',
+        camp: '/shared/camp.svg',
+    },
+}
+
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'how_quest_quil_work', label: 'How Quest Quil Work?' },
+    { href: '/', key: 'services', label: 'Services' },
+    { href: '/', key: 'pricing ', label: 'Pricing ' },
+    { href: '/', key: 'contact_us', label: 'Contact Us' },
+]
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+    '/camp/person-1.png',
+    '/camp/person-2.png',
+    '/camp/person-3.png',
+    '/camp/person-4.png',
+]
+
+// FEATURES SECTION
+export const FEATURES = [
+    {
+        title: 'Real maps can be offline',
+        icon: '/features/map.svg',
+        alt: 'map',
+        variant: 'green',
+        description:
+            'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
+    },
+    {
+        title: 'Set an adventure schedule',
+        icon: '/features/calendar.svg',
+        alt: 'calendar',
+        variant: 'green',
+        description:
+            "Schedule an adventure with friends. On holidays, there are many interesting offers from Quest Quil. That way, there's no more discussion",
+    },
+    {
+        title: 'Technology using augment reality',
+        icon: '/features/tech.svg',
+        alt: 'technology',
+        variant: 'green',
+        description:
+            'Technology uses augmented reality as a guide to your hiking trail in the forest to the top of the mountain. Already supported by the latest technology without an internet connection',
+    },
+    {
+        title: 'Many new locations every month',
+        icon: '/features/location.svg',
+        alt: 'location',
+        variant: 'orange',
+        description:
+            'Lots of new locations every month, because we have a worldwide community of climbers who share their best experiences with climbing',
+    },
+]
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+    {
+        title: 'Learn More',
+        links: [
+            'About Quest Quil',
+            'Press Releases',
+            'Environment',
+            'Jobs',
+            'Privacy Policy',
+            'Contact Us',
+        ],
+    },
+    {
+        title: 'Our Community',
+        links: [
+            'Climbing stonepeak',
+            'Hiking quest quil',
+            'Quest quil ridgeway',
+        ],
+    },
+]
+
+export const FOOTER_CONTACT_INFO = {
+    title: 'Contact Us',
+    links: [
+        { label: 'Admin Officer', value: '123-456-7890' },
+        { label: 'Email Officer', value: 'quest.quil@contact.com' },
+    ],
+}
+
+export const SOCIALS = {
+    title: 'Social',
+    links: [
+        'socials/facebook.svg',
+        'socials/instagram.svg',
+        'socials/twitter.svg',
+        'socials/youtube.svg',
+        'socials/wordpress.svg',
+    ],
+}
