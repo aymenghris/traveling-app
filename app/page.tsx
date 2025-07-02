a58a41bca0adbb8137c0232741e259098ca57748
aymenghris <aymengharis@gmail.com>
2025-07-02 10:40:03 +0100

    add main components to Home page for improved layout and functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
-export default function Home() {
-  return (
+import { Camp, Features, GetApp, Guide, Hero } from '@/components'
 
-    <div></div>
-  );
+export default function Home() {
+    return (
+        <>
+            <Hero />
+            <Camp />
+            <Guide />
+            <Features />
+            <GetApp />
+        </>
+    )
 }
